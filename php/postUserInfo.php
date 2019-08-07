4bfba678ff50c4bbc903355266adb529c958a0f0
Lee Jong Wan <lastffx@gmail.com>
2019-08-08 01:48:19 +0900

    declaration of variables used by other php pages

---
+++ php/postUserInfo.php
@@ -0,0 +1,11 @@
+<?php
+	$email = $_REQUEST['userMail'];
+	$name = $_POST['userName'];
+	$majorType = $_POST['userMajorType'];
+	$major = $_POST['userMajor'];
+	$interests = $_POST['userInterests'];
+	$code = $_POST['code'];
+	$password = $_POST['userPW'];
+	$hostAddr = $_POST['hostAddr'];
+	$verifyCode = $_REQUEST['verifyCode'];
+?>
