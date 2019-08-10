8d75b00a4839062a30791ba36d5dbb3df9f34e1a
Lee Jong Wan <lastffx@gmail.com>
2019-08-11 01:36:37 +0900

    hostname

--- java/com/example/untitledproject/ServerURL.java
+++ java/com/example/untitledproject/ServerURL.java
@@ -1,7 +1,7 @@
 package com.example.untitledproject;
 
 public class ServerURL {
-    public static String host = "http://192.168.0.16:4700/";
+    public static String host = "http://hostName/";
     public static String getHost() {
         return host;
     }
